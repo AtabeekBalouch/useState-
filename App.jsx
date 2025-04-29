1b136becdd88a69eccf0d7c134766e380c7e5d58
Atabeek Nawaz Balouch <80154117+AtabeekBalouch@users.noreply.github.com>
2025-04-29 21:04:43 +0500

    Update and rename README.md to App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Counter from '../../Project Learning/React Learning Lesson/useStateHook()/Counter.jsx'
+import Food from '../../Project Learning/React Learning Lesson/Beginner/Food.jsx'
+import MyComponent from './MyComponent.jsx'
+
+function App() { 
+
+  return (<>
+            <Counter/>
+            <MyComponent/>
+          </>);
+            
+}
+export default App# useState-
